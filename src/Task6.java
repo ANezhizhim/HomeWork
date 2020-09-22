04a40062bcd543030de82fc8f701cae9782fc531
A.Nezhizhim <A.Nezhizhim@x5.ru>
2020-09-22 13:09:50 +0300

    Задание 6,7 исправлено

--- src/Task6.java
+++ src/Task6.java
@@ -24,27 +24,8 @@ public class Task6 {
             }
 
         }
-        System.out.println(description );
-
-
-
-        /*
-        if (intA < 0) {
-            if (intA % 2 < 0) {
-                System.out.println("Отрицательное нечетное число");
-            } else
-                System.out.println("Отрицательное четное число");
-        } else if (intA > 0) {
-
-            if (intA % 2 > 0) {
-                System.out.println("Положительное нечетное число");
-            } else
-                System.out.println("Положительное четное число");
-        } else System.out.println("Нулевое число");
-        */
+        System.out.println(description);
     }
-
-
 }
 
 /* Замечание:
--- src/Task7.java
+++ src/Task7.java
@@ -5,16 +5,16 @@ public class Task7 {
 
     public static void main(String[] args) {
         int intCode;
-        float fltCost=0.0f;
+        float fltCost=0.0f, fltTime=10.0f;
         String strTown="";
         System.out.println("Введите целое число: ");
         Scanner scan1 = new Scanner(System.in);
         intCode  = scan1.nextInt();
         switch (intCode){
-            case 905: fltCost =4.1f*10.0f ; strTown="Москва"; break;
-            case 194: fltCost =5.0f*10.0f ; strTown="Ростов"; break;
-            case 491: fltCost =2.69f*10.0f ; strTown="Краснодар"; break;
-            case 800: fltCost =5.0f*10.0f ; strTown="Киров"; break;
+            case 905: fltCost =4.1f*fltTime ; strTown="Москва"; break;
+            case 194: fltCost =5.0f*fltTime ; strTown="Ростов"; break;
+            case 491: fltCost =2.69f*fltTime ; strTown="Краснодар"; break;
+            case 800: fltCost =5.0f*fltTime ; strTown="Киров"; break;
 
 
         }
