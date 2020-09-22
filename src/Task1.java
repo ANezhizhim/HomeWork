e25a501129e7ec244fc3c20e7c29bc6f96e7e9fd
A.Nezhizhim <A.Nezhizhim@x5.ru>
2020-09-22 09:15:48 +0300

    исправлено имя переменной на "а"

--- src/Task1.java
+++ src/Task1.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        int intA;
+        int a;
         System.out.println("Введите целое число: ");
         Scanner ExprScan = new Scanner(System.in);
-        intA = ExprScan.nextInt();
-        System.out.println("Ответ: "+ intA % 10);
+        a = ExprScan.nextInt();
+        System.out.println("Ответ: "+ a % 10);
 
 
     }
