7707f3876ec6b6446eb7033124155c18f0fc537f
A.Nezhizhim <A.Nezhizhim@x5.ru>
2020-09-22 14:28:18 +0300

    Задания исправлены

--- src/Task7.java
+++ src/Task7.java
@@ -5,19 +5,31 @@ public class Task7 {
 
     public static void main(String[] args) {
         int intCode;
-        float fltCost=0.0f, fltTime=10.0f;
-        String strTown="";
+        float fltCost = 0.0f, fltTime = 10.0f;
+        String strTown = "";
         System.out.println("Введите целое число: ");
         Scanner scan1 = new Scanner(System.in);
-        intCode  = scan1.nextInt();
-        switch (intCode){
-            case 905: fltCost =4.1f*fltTime ; strTown="Москва"; break;
-            case 194: fltCost =5.0f*fltTime ; strTown="Ростов"; break;
-            case 491: fltCost =2.69f*fltTime ; strTown="Краснодар"; break;
-            case 800: fltCost =5.0f*fltTime ; strTown="Киров"; break;
+        intCode = scan1.nextInt();
+        switch (intCode) {
+            case 905:
+                fltCost = 4.1f * fltTime;
+                strTown = "Москва";
+                break;
+            case 194:
+                fltCost = 5.0f * fltTime;
+                strTown = "Ростов";
+                break;
+            case 491:
+                fltCost = 2.69f * fltTime;
+                strTown = "Краснодар";
+                break;
+            case 800:
+                fltCost = 5.0f * fltTime;
+                strTown = "Киров";
+                break;
 
 
         }
-        System.out.println(strTown+". Стоимость разговора:" + fltCost);
+        System.out.println(strTown + ". Стоимость разговора :" + fltCost);
     }
 }
