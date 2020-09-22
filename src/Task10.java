d5a6cae8668815a6432b38d93f25fa3d73e94f75
A.Nezhizhim <A.Nezhizhim@x5.ru>
2020-09-22 14:19:01 +0300

    Задания исправлены

--- src/Task10.java
+++ src/Task10.java
@@ -2,21 +2,30 @@ public class Task10 {
 
     public static void main(String[] args) {
 
-        int[] intArr  ={15,10,0,-6,-5,3,0,-34,0,32,56,0,24,-52};
-        int EndNum;
+        int[] intArr = {15, 10, 0, -6, -5, 3, 0, -34, 0, 32, 56, 0, 24, -52};
+        int endNum = intArr.length - 1;
 
-        for (int i=0; i<intArr.length;i++){
-            if(intArr[i]==0){
-                  EndNum=intArr.length;//запоминаем максимальный индекс массива
-                while ((--EndNum>i)&(intArr[EndNum]==0)){}// ищем с конца ближайший ненулевой элемент
-                intArr[i]=intArr[EndNum];// замена нулевого элемента
-                intArr[EndNum]=0;
+        for (int i = 0; i < endNum; i++) {
+            if (intArr[i] == 0) {
+                if (intArr[endNum] != 0) {
+                    intArr[i] = intArr[endNum];
+                    intArr[endNum] = 0;
+                } else {
+                    i--;
+                }
+                endNum--;
             }
         }
-        for (int i=0; i<intArr.length;i++){
-            System.out.println("Index["+i+"]"+intArr[i]);
+        for (int i = 0; i < intArr.length; i++) {
+            System.out.println("Index[" + i + "]" + intArr[i]);
         }
-
-
     }
 }
+/*Замечание:
+•	Задание 10: Вот это что-то очень сложное while ((--EndNum>i)&(intArr[EndNum]==0)){}.
+ Подумайте над более простым решением
+
+10. Дан массив целых чисел:
+[15,10,0,-6,-5,3,0,-34,0,32,56,0,24,-52]. Переместить
+нули в конец массива. Вывести результат в консоль
+*/
