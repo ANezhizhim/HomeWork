92fe4e46167d4e0e9f0328f83d1367e6616e8285
A.Nezhizhim <A.Nezhizhim@x5.ru>
2020-09-22 10:01:51 +0300

    исправлено имя переменной на "а", исправлено название метода на "exprScan" Задание 2 для любого числа знаков через циклы получилось еще короче, чем ранее

--- src/Task2.java
+++ src/Task2.java
@@ -3,20 +3,27 @@ import java.util.Scanner;
 public class Task2 {
 
     public static void main(String[] args) {
-        int a1, a2, a3, intA;
-        System.out.println("Введите целое 3-х значное число и в ответ получите их сумму: ");
-        Scanner ExprScan = new Scanner(System.in);
-        intA = ExprScan.nextInt();
-        int Res;
-        a1 = (intA / 100);
-        a2 = (intA / 10) % 10;
-        a3 = intA % 10;
-
-
-        Res = a1 + a2 + a3;
-        System.out.println("a1 " + a1);
-        System.out.println("a2 " + a2);
-        System.out.println("a3 " + a3);
-        System.out.println("Ответ: " + Res);
+        int  a;
+        System.out.println("Введите целое число и в ответ получите сумму цифр: ");
+        Scanner exprScan = new Scanner(System.in);
+        a = exprScan.nextInt();
+        int res=0;
+        while ((a>0)) {
+            res+=a%10;
+            a=a/10;
+        }
+        System.out.println("Ответ: " + res);
     }
 }
+/*Замечание:
+
+•	Задание 2: Нужно продумать решение через циклы, так как данный вариант
+реализации для 10-тизначного числа будет уже не так прост в реализации
+ */
+/*
+Задание:
+
+2. Ввести целое трехзначное число в консоли и найти
+сумму цифр этого трехзначного числа
+
+ */
--- src/Task9.java
+++ src/Task9.java
@@ -5,14 +5,13 @@ public class Task9 {
 
       int[] intArr  ={15,10,51,-6,-5,3,-10,-34,0,32,56,-12,24,-52};
       int intBuff, maxIndex=intArr.length-1;
-      int Num=(intArr.length)/2;
+      int num=(intArr.length)/2;
 
-        for (int i=0; i<Num;i++){
+        for (int i=0; i<num;i++){
             intBuff=intArr[i];
             intArr[i]=intArr[maxIndex-i];
             intArr[maxIndex-i]=intBuff;
-            //System.out.println(Num);
-            //System.out.println(intArr[i]);
+
         }
         for (int i=0; i<intArr.length;i++){
             System.out.println("Index["+i+"]"+intArr[i]);
@@ -20,3 +19,7 @@ public class Task9 {
 
     }
 }
+/* Замечания:
+
+•	Задание 9: Убрать закомментированный код. Переменные с большой буквы не называем
+ */
