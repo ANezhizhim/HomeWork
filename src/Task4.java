04684e8e2dd34df6c77af87b730366d37b057ea1
A.Nezhizhim <A.Nezhizhim@x5.ru>
2020-09-22 14:31:14 +0300

    Задания исправлены

--- src/Task4.java
+++ src/Task4.java
@@ -11,7 +11,9 @@ public class Task4 {
             a++;
         } else if (a < 0) {
             a -= 2;
-        } else a = 10;
+        } else {
+            a = 10;
+        }
 
 
         System.out.println("Ответ: " + a);
