bc79dae1d203c1b3b1f45f40c59a14f638c89cbe
A.Nezhizhim <A.Nezhizhim@x5.ru>
2020-10-06 09:55:12 +0300

    Задания исправлены

--- src/Task1.java
+++ src/Task1.java
@@ -2,16 +2,20 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        int a;
+        int input;
         System.out.println("Введите целое число: ");
         Scanner exprScan = new Scanner(System.in);
-        a = exprScan.nextInt();
-        System.out.println("Ответ: " + a % 10);
+        input = exprScan.nextInt();
+        System.out.println("Ответ: " + input % 10);
 
 
     }
 }
-/* Замечания:
+/* Замечания2:
+Задание 1: Вы ввели переменную a, лучшне стало, почему бы ее не назвать, например, input? Так же будет намного понятней если код разрастется.
+ */
+
+/* Замечания1:
 •	Переменные и методы должны иметь говорящие названия, исключения – циклы (int i, j –счетчики циклов и это понятно). Что значит intA? Актулаьно для всех заданий
 •	Названия методов и переменных не должно быть с большой буквы. Вместо ExprScan должно быть exprScan и так далее. Проверьте все задания
 
